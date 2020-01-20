272a0dfa13944d3045af61e77daf071387485b34
Derrik Campbell <derrikcampbell@hotmail.com>
2020-01-20 18:11:07 -0500

    ovies cms setup up to week 3

---
+++ admin/scripts/read.php
@@ -0,0 +1,56 @@
+<?php
+
+function getAll($tbl){
+    $pdo = Database::getInstance()->getConnection();
+    $queryAll = 'SELECT * FROM ' .$tbl;
+    $results = $pdo->query($queryAll);
+
+    if($results){
+        return $results;
+    }else{
+       return ' There was a problem accesing this info';
+        }
+
+}
+
+function getSingleMovie($tbl, $col, $id){
+    //TODO: finish the function base on getAll, this time only return
+    //one movie's field
+    $pdo = Database::getInstance()->getConnection();
+
+    //singlequote
+    // $querySingle = 'SELECT * FROM ' .$tbl.' WHERE '.$col.' = '.$id;
+    $querySingle = "SELECT * FROM $tbl WHERE $col = $id";
+    $results = $pdo->query($querySingle);
+    
+    if($results){
+        return $results;
+    }else{
+       return ' There was a problem accesing this info';
+        }
+}
+
+function getMoviesByFilter($args){
+    $pdo = Database::getInstance()->getConnection();
+
+    $filterQuery = 'SELECT * FROM '.$args['tbl'].' AS t, '.$args['tbl2'].' AS t2, '.$args['tbl3'] . ' AS t3';
+    $filterQuery .= ' WHERE t. ' .$args['col']. ' = t3.' .$args['col'];
+    $filterQuery .= ' AND t2. ' .$args['col2']. '= t3.' .$args['col2'];
+    $filterQuery .= ' AND t2. ' .$args['col3']. '= "' .$args['filter']. '"';
+
+    
+    //check in phpmyadmin
+    // echo $filterQuery;
+    // exit;
+    
+
+
+    $results = $pdo->query($filterQuery);
+
+    if($results){
+        return $results;
+    }else{
+        return 'There was a problem accessing info';
+    }
+    
+}
